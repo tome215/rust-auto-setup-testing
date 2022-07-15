9f1c2e11b6f54e37dbb829ab78153494d006ad9f
Tomislav Žabčić-Matić <tome215@gmail.com>
2022-07-15 07:16:39 -0400

    rust auto-setup attempt 6

--- rustSetup.sh
+++ rustSetup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo su root -c 'cd /root && echo -e "\n\nexport RUSTUP_HOME=/usr/local\nexport CARGO_HOME=/usr/local" >> .bashrc'
-sudo su root -c 'cd /root && source .bashrc && curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path'
+sudo su root -c 'cd /root && export RUSTUP_HOME=/usr/local && export CARGO_HOME=/usr/local && curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path'
